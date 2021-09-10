ef21ffa4578a05f79d2e925bc4bcacf09bb4a80d
anjanakg <anjanakg032@gmail.com>
2021-09-10 12:28:34 +0530

    if else condition

---
+++ javascript/fizbuzdivisible.js
@@ -0,0 +1,13 @@
+var num=0;
+if(num==0){
+console.log('');
+}
+else if(num%15==0){
+    console.log('FizBuz');
+}
+else if(num%3==0){
+    console.log('Fiz');
+}
+else if(num%5==0){
+    console.log('Buz');
+}
---
+++ javascript/oddoreven.js
@@ -0,0 +1,10 @@
+var num=19768;
+if (num==0){
+    console.log('number is 0');
+}
+else if(num%2==0){
+   console.log(`${num} is a even number`);
+}
+else{
+    console.log(`${num} is a odd number`);
+}
